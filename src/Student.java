7c131010d189525bc249bd5a79ae9a77fa7ac2bb
Slow_Time <1395881075@qq.com>
2017-12-14 16:58:43 +0800

    增加学生工厂

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,28 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Student {
+    String sid;
+    String name;
+    String sex;
+    LocalDate birthDate;
+    String id;
+    String major;
+    LocalDate inDate;
+    String phone;
+    String xueji;
+    ArrayList<String> scores;
+
+    public Student(String sid, String name, String sex, LocalDate birthDate, String id, String major, LocalDate inDate, String phone, String xueji, ArrayList<String> scores) {
+        this.sid = sid;
+        this.name = name;
+        this.sex = sex;
+        this.birthDate = birthDate;
+        this.id = id;
+        this.major = major;
+        this.inDate = inDate;
+        this.phone = phone;
+        this.xueji = xueji;
+        this.scores = scores;
+    }
 }
---
+++ src/StudentFactory.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class StudentFactory {
+    public static ArrayList<Student> getStudents() {
+        ArrayList<Student> students = new ArrayList<>();
+        return students;
+    }
+}
